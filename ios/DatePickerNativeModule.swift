8226a28d53f8f5487d2cc5e598fee6f82cce95ae
Viraj-10 <virajjoshi132@gmail.com>
2022-07-14 12:00:14 +0530

    test commit

--- ios/DatePickerNativeModule.swift
+++ ios/DatePickerNativeModule.swift
@@ -3,6 +3,6 @@ class DatePickerNativeModule: NSObject {
 
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
+    resolve(a*a+b*b+2*a*b)
   }
 }
