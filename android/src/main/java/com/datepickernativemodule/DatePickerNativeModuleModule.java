62fe4c893621b5381cff12b5a4904ad21d7911dc
Viraj-10 <virajjoshi132@gmail.com>
2022-07-14 11:44:27 +0530

    chore: initial commit

---
+++ android/src/main/java/com/datepickernativemodule/DatePickerNativeModuleModule.java
@@ -0,0 +1,33 @@
+package com.datepickernativemodule;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = DatePickerNativeModuleModule.NAME)
+public class DatePickerNativeModuleModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "DatePickerNativeModule";
+
+    public DatePickerNativeModuleModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(double a, double b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+}
